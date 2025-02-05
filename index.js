c779889a1ea4bf5c73f6bf08c5b41945a2da67a2
Clarissa Rodriguez <clarissa.rodriguez@siteminder.com>
2025-02-05 13:51:29 +0800

    add pipes

--- index.js
+++ index.js
@@ -1,15 +1,51 @@
 const fs = require('fs')
+const csv = require('csvtojson')
+const { Transform } = require('stream')
 
 const main = async () => {
   const readStream = fs.createReadStream('./data/import.csv', {
-    highWaterMark: 100,
+    highWaterMark: 50,
   }) 
 
   const writeStream = fs.createWriteStream('./data/export.csv')
 
-  readStream.pipe(writeStream)
+  const myTransform = new Transform({
+    objectMode: true,
+    transform(chunk, enc, callback) {
+      const user = {
+        name: chunk.name,
+        email: chunk.email.toLowerCase(),
+        age: Number(chunk.age),
+        salary: Number(chunk.salary),
+        isActive: chunk.isActive === 'true',
+      }
 
-  readStream.on('end', () => console.log('Read stream ended'))
+      callback(null, user) // callback('Some error')
+    }
+  })
+  
+  const myFilter = new Transform({
+    objectMode: true,
+    transform(user, enc, callback) {
+      if(!user.isActive || user.salary < 1000) {
+        callback(null)
+        return
+      }
+
+      callback(null, user)
+    }
+  })
+
+  readStream
+    .pipe(csv(
+      { delimiter: ';' },
+      { objectMode: true },
+    ))
+    .pipe(myTransform)
+    .pipe(myFilter)
+    .on('data', data => console.log('>>> DATA:\n', data))
+    .on('error', error => console.error('Stream error: ', error))
+    .on('end', () => console.log('Read stream ended'))
 
   writeStream.on('finish', () => console.log('Write stream finished'))
 }
